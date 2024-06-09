c187e71c451e6cbc830adfb029cac41401677380
Yehor <163127511+YehorHaikevych@users.noreply.github.com>
2024-06-09 12:21:22 +0300

    add index file

---
+++ js/task-2.js
@@ -0,0 +1,9 @@
+function getShippingMessage(country, price, deliveryFee) {
+  const total = `Shipping to ${country} will cost ${
+    price + deliveryFee
+  } credits`;
+  return total;
+}
+console.log(getShippingMessage("Australia", 120, 50));
+console.log(getShippingMessage("Germany", 80, 20));
+console.log(getShippingMessage("Sweden", 100, 20));
